58ad83ec613306bb93c7c4402ad48f1ffcc76bcd
yfy227 <110282123+yfy227@users.noreply.github.com>
2025-04-29 02:18:59 +0800

    Add files via upload

---
+++ 测试样本.cpp
@@ -0,0 +1,11 @@
+#include "正式版v1.0.h"
+
+int main() {
+    // 启动保护机制
+    self_protection::ProtectionManager protection;
+
+  
+    std::this_thread::sleep_for(std::chrono::minutes(10)); // 模拟运行时间
+
+    return 0;
+}
